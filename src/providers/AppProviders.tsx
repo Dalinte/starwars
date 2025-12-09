e8cb369240bb6466ccc31b2a8a5f7c4a5a3a00ed
Dalinte <kollekcionerssd1@gmail.com>
2025-12-09 13:26:05 +0800

    fix drawer size

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/configs';
 import type { ReactNode } from 'react';
-import { SideDrawerManager } from '@/components/SlideDrawer/SideDrawerManager';
+import { SideDrawerManager } from '@/components/slideDrawer/SideDrawerManager';
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
